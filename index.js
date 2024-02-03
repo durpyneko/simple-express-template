b817e1932bcec464be4a73120fbc93f1ad820d4b
durpyneko <blackninja36@protonmail.com>
2024-02-03 14:03:47 +0200

    upload files

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.use("/public", express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
